a402212024e0b3bc26131de3b632e93c5850f802
Rimanec <denis.timofeev.dtv@gmail.com>
2020-04-16 16:05:09 +0300

    3.3. Разбор первой программы.[#274648]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main( String[] args) {
+        System.out.println(" Denis Timofeev");
+        System.out.println(" 19.05.91");
+
+    }
+}
