33c5a817a4957b82f0c76b83476f20ba7462da0f
remanec <denis.timofeev.dtv@gmail.com>
2020-04-25 22:51:42 +0300

    0. Операции сравнения [#274594]

---
+++ src/ru/job4j/ru/job4j/condition/Liken.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+
+        boolean result1 = first > second; // first больше second?
+        boolean result2 = first < second;
+        boolean result3 = first == second;
+
+                System.out.println(result1);
+                System.out.println(result2);
+                System.out.println(result3);
+    }
+}
